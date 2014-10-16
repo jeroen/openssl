a0ef0651ead38b3715c213bf5a7f5cddcd600f76
Jeroen <jeroenooms@gmail.com>
2014-10-16 15:50:57 -0700

    Rename package

--- R/openssl.R
+++ R/openssl.R
@@ -1,7 +1,28 @@
-rand_pseudo_bytes <- function(length = 1){
-  .Call(R_RAND_pseudo_bytes, length, TRUE)
+# Generate random bytes with OpenSSL
+#
+# These functions interface to the OpenSSL random number generators. They
+# can generate crypto secure random numbers in R.
+#
+#' @title Generate random bytes with OpenSSL
+#' @rdname rand_pseudo_bytes
+#' @useDynLib openssl R_RAND_pseudo_bytes
+#' @param n how many bytes to generate
+#' @export
+#' @references OpenSSL manual: \url{https://www.openssl.org/docs/crypto/RAND_bytes.html}
+#' @examples rnd <- rand_bytes(10)
+#' as.numeric(rnd)
+#' as.character(rnd)
+#' as.logical(rawToBits(rnd))
+#'
+#' # numbers range from 0 to 255
+#' rnd <- rand_bytes(100000)
+#' hist(as.numeric(rnd), breaks=-1:255)
+rand_pseudo_bytes <- function(n = 1){
+  .Call(R_RAND_pseudo_bytes, n, TRUE)
 }
 
-rand_bytes <- function(length = 1){
-  .Call(R_RAND_pseudo_bytes, length, FALSE)
+#' @rdname rand_pseudo_bytes
+#' @export
+rand_bytes <- function(n = 1){
+  .Call(R_RAND_pseudo_bytes, n, FALSE)
 }
--- src/rand_bytes.c
+++ src/rand_bytes.c
@@ -2,22 +2,22 @@
 #include <R.h>
 #include <Rinternals.h>
 
-SEXP R_RAND_pseudo_bytes(SEXP length, SEXP pseudo) {
+SEXP R_RAND_pseudo_bytes(SEXP n, SEXP pseudo) {
   int use_pseudo = asLogical(pseudo);
-  int num = asInteger(length);
+  int length = asInteger(n);
   int result;
-  unsigned char buf[num];
+  unsigned char buf[length];
   if(use_pseudo){
-    result = RAND_pseudo_bytes(buf, num);
+    result = RAND_pseudo_bytes(buf, length);
   } else {
-    result = RAND_bytes(buf, num);
+    result = RAND_bytes(buf, length);
   }
   if(!result) {
     error("Failed to generated pseudo random bytes.");
   }
 
-  SEXP out = PROTECT(allocVector(RAWSXP, num));
-  for (int i = 0; i < num; i++) {
+  SEXP out = PROTECT(allocVector(RAWSXP, length));
+  for (int i = 0; i < length; i++) {
     RAW(out)[i] = buf[i];
   }
   UNPROTECT(1);

d8ec49082d8a8f1be886f723deb4ee7d2be677e0
Jeroen Ooms <jeroenooms@gmail.com>
2014-10-20 14:21:39 -0700

    Trigger CI

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,4 +1,4 @@
-# There openssl libraries were compiled with the Rtools gcc tool chain.
+# Build against openssl libraries that were compiled with the Rtools gcc toolchain.
 setInternet2()
 download.file("http://www.stat.ucla.edu/~jeroen/files/openssl-1.0.1j.zip", "lib.zip", quiet = TRUE)
 dir.create("../windows", showWarnings = FALSE)

ade9a8517691cda88fe2a6bdec379d68908edac3
Jeroen Ooms <jeroenooms@gmail.com>
2014-10-20 12:10:15 -0700

    Move script

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,3 +1,4 @@
+# There openssl libraries were compiled with the Rtools gcc tool chain.
 setInternet2()
 download.file("http://www.stat.ucla.edu/~jeroen/files/openssl-1.0.1j.zip", "lib.zip", quiet = TRUE)
 dir.create("../windows", showWarnings = FALSE)

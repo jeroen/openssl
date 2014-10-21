35eeecf31c51c29aea21bfa302485c0479a317a3
Jeroen Ooms <jeroenooms@gmail.com>
2014-10-21 10:36:22 -0700

    Update windows binaries URL

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,5 +1,5 @@
 # Build against openssl libraries that were compiled with the Rtools gcc toolchain.
 setInternet2()
-download.file("http://www.stat.ucla.edu/~jeroen/files/openssl-1.0.1j.zip", "lib.zip", quiet = TRUE)
+download.file("http://jeroenooms.github.io/openssl/windows/openssl-1.0.1j.zip", "lib.zip", quiet = TRUE)
 dir.create("../windows", showWarnings = FALSE)
 unzip("lib.zip", exdir = "../windows")

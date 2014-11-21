f728072d48e4669ad94f93c0969b8a470fce3f5e
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-20 16:03:35 -0800

    Clean up documentation

--- R/openssl.R
+++ R/openssl.R
@@ -6,7 +6,7 @@
 #' @title Generate random bytes with OpenSSL
 #' @rdname rand_bytes
 #' @useDynLib openssl R_RAND_bytes
-#' @param n number of random bytes to generate
+#' @param n number of random bytes or numbers to generate
 #' @export
 #' @references OpenSSL manual: \url{https://www.openssl.org/docs/crypto/RAND_bytes.html}
 #' @examples rnd <- rand_bytes(10)
@@ -14,12 +14,19 @@
 #' as.character(rnd)
 #' as.logical(rawToBits(rnd))
 #'
-#' # numbers range from 0 to 255
+#' # bytes range from 0 to 255
 #' rnd <- rand_bytes(100000)
 #' hist(as.numeric(rnd), breaks=-1:255)
 #'
 #' # Generate random doubles between 0 and 1
 #' rand_num(5)
+#'
+#' # Use CDF to map [0,1] into random draws from a distribution
+#' x <- qnorm(rand_num(1000), mean=100, sd=15)
+#' hist(x)
+#'
+#' y <- qbinom(rand_num(1000), size=10, prob=0.3)
+#' hist(y)
 rand_bytes <- function(n = 1){
   .Call(R_RAND_bytes, n, FALSE)
 }

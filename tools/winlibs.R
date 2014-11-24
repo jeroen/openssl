76b3297fca6b52264140b450739c192b605944a9
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-23 17:38:26 -0800

    Fix up library

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,5 +1,8 @@
 # Build against openssl libraries that were compiled with the Rtools gcc toolchain.
-setInternet2()
-download.file("http://jeroenooms.github.io/openssl/windows/openssl-1.0.1j.zip", "lib.zip", quiet = TRUE)
-dir.create("../windows", showWarnings = FALSE)
-unzip("lib.zip", exdir = "../windows")
+if(!file.exists("../windows/openssl-1.0.1j/include/openssl/ssl.h")){
+  setInternet2()
+  download.file("http://jeroenooms.github.io/openssl/windows/openssl-1.0.1j.zip", "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}

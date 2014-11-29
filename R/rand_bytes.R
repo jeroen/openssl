e3e1332083b0e0affa95791c8967cbe55b21c3a3
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-29 13:22:04 -0800

    Rename files

--- R/rand_bytes.R
+++ R/rand_bytes.R
@@ -36,3 +36,11 @@ rand_bytes <- function(n = 1){
 rand_pseudo_bytes <- function(n = 1){
   .Call(R_RAND_bytes, n, TRUE)
 }
+
+#' @rdname rand_bytes
+#' @export
+rand_num <- function(n = 1){
+  # 64 bit double requires 8 bytes.
+  x <- matrix(as.numeric(rand_bytes(n*8)), ncol = 8)
+  as.numeric(x %*% 256^-(1:8))
+}
--- R/random_num.R
+++
@@ -1,7 +0,0 @@
-#' @rdname rand_bytes
-#' @export
-rand_num <- function(n = 1){
-  # 64 bit double requires 8 bytes.
-  x <- matrix(as.numeric(rand_bytes(n*8)), ncol = 8)
-  as.numeric(x %*% 256^-(1:8))
-}

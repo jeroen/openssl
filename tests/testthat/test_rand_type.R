197e09c62e48e258273b2679a7d18f732b53a829
ironholds <ironholds@gmail.com>
2014-11-29 17:07:21 -0500

    start error handlers

---
+++ tests/testthat/test_rand_type.R
@@ -0,0 +1,13 @@
+context("Test the error handlers for input type")
+
+test_that("rand_bytes detects non-numeric arguments", {
+
+  expect_that(rand_bytes("turnip"), throws_error(regexp = "Please provide a numeric or integer value for n", fixed = TRUE))
+
+})
+
+test_that("pseudo_rand_bytes detects non-numeric arguments", {
+
+  expect_that(pseudo_rand_bytes("turnip"), throws_error(regexp = "Please provide a numeric or integer value for n", fixed = TRUE))
+
+})

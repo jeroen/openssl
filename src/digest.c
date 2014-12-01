9135b5f7db4b0fcfce996657d54035ed9ae371a7
Jeroen Ooms <jeroenooms@gmail.com>
2014-11-30 16:22:00 -0800

    Refactor C code

--- src/digest.c
+++ src/digest.c
@@ -7,10 +7,9 @@
  * Adapted from example at: https://www.openssl.org/docs/crypto/EVP_DigestInit.html
  */
 
-SEXP digest_string(const char *x, const char *algo, int string){
+unsigned int digest_string(const char *x, const char *algo, unsigned char *md_value) {
 
   /* init openssl stuff */
-  unsigned char md_value[EVP_MAX_MD_SIZE];
   unsigned int md_len;
   const EVP_MD *md = EVP_get_digestbyname(algo);
   if(!md)
@@ -22,26 +21,7 @@ SEXP digest_string(const char *x, const char *algo, int string){
   EVP_DigestUpdate(mdctx, x, strlen(x));
   EVP_DigestFinal_ex(mdctx, md_value, &md_len);
   EVP_MD_CTX_destroy(mdctx);
-
-  /* create outputs */
-  SEXP out;
-  if(string){
-    char mdString[2*md_len+1];
-    for (int i = 0; i < md_len; i++) {
-      sprintf(&mdString[i*2], "%02x", (unsigned int) md_value[i]);
-    }
-    mdString[2*md_len+1] = '\0';
-    out = PROTECT(mkChar(mdString));
-  } else {
-    out = PROTECT(allocVector(RAWSXP, md_len));
-    for (int i = 0; i < md_len; i++) {
-      RAW(out)[i] = md_value[i];
-    }
-  }
-
-  /* finish up */
-  UNPROTECT(1);
-  return out;
+  return md_len;
 }
 
 SEXP R_digest_raw(SEXP x, SEXP algo){
@@ -49,7 +29,17 @@ SEXP R_digest_raw(SEXP x, SEXP algo){
   if(!isString(x))
     error("Argument 'x' must be a character vector.");
 
-  return digest_string(CHAR(asChar(x)), CHAR(asChar(algo)), 0);
+  /* create hash */
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+  unsigned int md_len = digest_string(CHAR(asChar(x)), CHAR(asChar(algo)), md_value);
+
+  /* create raw vector */
+  SEXP out = PROTECT(allocVector(RAWSXP, md_len));
+  for (int i = 0; i < md_len; i++) {
+    RAW(out)[i] = md_value[i];
+  }
+  UNPROTECT(1);
+  return out;
 }
 
 SEXP R_digest(SEXP x, SEXP algo){
@@ -59,10 +49,19 @@ SEXP R_digest(SEXP x, SEXP algo){
     error("Argument 'algo' must be a character vector.");
 
   int len = length(x);
-  const char* alg = CHAR(asChar(algo));
   SEXP out = PROTECT(allocVector(STRSXP, len));
   for (int i = 0; i < len; i++) {
-    SET_STRING_ELT(out, i, digest_string(CHAR(STRING_ELT(x, i)), alg, 1));
+    /* create hash */
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len = digest_string(CHAR(STRING_ELT(x, i)), CHAR(asChar(algo)), md_value);
+
+    /* create character vector */
+    char mdString[2*md_len+1];
+    for (int i = 0; i < md_len; i++) {
+      sprintf(&mdString[i*2], "%02x", (unsigned int) md_value[i]);
+    }
+    mdString[2*md_len+1] = '\0';
+    SET_STRING_ELT(out, i, mkChar(mdString));
   }
   UNPROTECT(1);
   return out;

c71e7debad04fd5a3334c2ac9914a5a46b01236b
ironholds <ironholds@gmail.com>
2014-12-01 12:47:17 -0500

    Move error checking and salt generation into a sub-function, preparing to split hash() out into different functions. Remove call to anyNA, which isn't present <= R3.0.2, to avoid artificially limiting the acceptable R versions for this library.

--- R/hash.R
+++ R/hash.R
@@ -9,8 +9,8 @@
 #' @param x a string, or a vector of strings, to hash.
 #' @param algo the cryptographic algorithm to apply. Options are "md5", "sha", "sha1",
 #' "sha224","sha256", "sha384", "sha512", "ripemd160" and "dss1". See "Details".
-#' @param rand_val whether or not to include a cryptographically strong randomised value,
-#' generated with \code{\link{rand_bytes}}, with each string. This string is consistent
+#' @param salt whether or not to include a cryptographically strong randomised value (or "salt"),
+#' generated with \code{\link{rand_bytes}}, with each string. This salt is consistent
 #' within the call to \code{hash}, but inconsistent between runs, allowing you to
 #' maintain consistency within a dataset while simultaneously rendering the results
 #' incomparable to other datasets with some of the same data points. Set to FALSE by default.
@@ -28,21 +28,10 @@
 #' @examples library(digest)
 #' hash("foo", "md5")
 #' digest("foo", "md5", serialize = FALSE)
-hash <- function(x, algo, rand_val = FALSE){
+hash <- function(x, algo, salt = FALSE){
 
-  #Check type, presence of NA values
-  if(any(is.list(x),is.data.frame(x))){
-    warning("x must be a vector. Attempting to convert.")
-    x <- unlist(x)
-  }
-  if(anyNA(x)){
-    warning("x contains NA values (possibly from conversion).")
-  }
-
-  #Is a random value desired?
-  if(rand_val){
-    x <- paste0(x,rand_bytes(1))
-  }
+  #Checks
+  x <- hash_checks(x,salt)
 
   #Call and return
   .Call(R_digest, as.character(x), as.character(algo))
@@ -52,4 +41,24 @@ hash <- function(x, algo, rand_val = FALSE){
 #' @export
 rawhash <- function(x, algo){
   .Call(R_digest_raw, as.character(x), as.character(algo))
+}
+
+hash_checks <- function(x,salt){
+
+  #Check type, presence of NA values
+  if(any(is.list(x),is.data.frame(x))){
+    warning("x must be a vector. Attempting to convert.")
+    x <- unlist(x)
+  }
+  if(any(is.na(x))){
+    warning("x contains NA values (possibly from conversion).")
+  }
+
+  #Is a random value desired?
+  if(salt){
+    x <- paste0(x,rand_bytes(1))
+  }
+
+  #Return
+  return(x)
 }

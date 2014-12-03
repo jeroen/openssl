6a138bb2adef18339c395d5b003fd73ba8e6fbd5
ironholds <ironholds@gmail.com>
2014-12-03 10:06:15 -0500

    update error-handling tests

--- tests/testthat/test_hash_error.R
+++ tests/testthat/test_hash_error.R
@@ -1,26 +1,13 @@
 context("Test the error handlers for hash functions")
 
-test_that("hash detects invalid algorithms", {
+test_that("non-vector inputs are detected", {
 
-  expect_that(hash("foo","turnip"), throws_error(regexp = "Unknown cryptographic algorithm", fixed = TRUE))
-
-})
-
-test_that("rawhash detects invalid algorithms", {
-
-  expect_that(rawhash("foo","turnip"), throws_error(regexp = "Unknown cryptographic algorithm", fixed = TRUE))
+  expect_that(md5(list(c("foo","bar"),"baz")), throws_error(regexp = "must be raw or character vector", fixed = TRUE))
 
 })
 
-test_that("hash detects non-vector inputs", {
+test_that("non-character inputs are detected", {
 
-  expect_that(hash(list(c("foo","bar"),"baz"),"md5"), gives_warning(regexp = "must be a vector. Attempting to convert.", fixed = TRUE))
-
-})
-
-test_that("Vectors containing NA values are detected", {
-
-  expect_that(hash(NA,"md5"),
-              gives_warning(regexp = "x contains NA values (possibly from conversion)", fixed = TRUE))
+  expect_that(md5(12), throws_error(regexp = "must be raw or character vector", fixed = TRUE))
 
 })

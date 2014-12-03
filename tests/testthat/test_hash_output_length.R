125fd2d68fa81c467e46688dbf83dc2a838ec44a
ironholds <ironholds@gmail.com>
2014-12-03 10:11:03 -0500

    update output length and value tests to enclude new cryptographic algorithms

--- tests/testthat/test_hash_output_length.R
+++ tests/testthat/test_hash_output_length.R
@@ -1,25 +1,38 @@
 context("Test the length of the output of various algorithms.")
 
+test_that("MD4 outputs a 32-character hash", {
+
+  expect_that(nchar(md4("foo")), equals(32))
+
+})
+
 test_that("MD5 outputs a 32-character hash", {
 
-  expect_that(nchar(hash("foo","md5")), equals(32))
+  expect_that(nchar(md5("foo")), equals(32))
+
+})
+
+test_that("ripemd160 outputs a 40-character hash", {
+
+  expect_that(nchar(ripemd160("foo")), equals(40))
 
 })
 
 test_that("SHA1 outputs a 40-character hash", {
 
-  expect_that(nchar(hash("foo","sha1")), equals(40))
+  expect_that(nchar(sha1("foo")), equals(40))
 
 })
 
+
 test_that("SHA256 outputs a 64-character hash", {
 
-  expect_that(nchar(hash("foo","sha256")), equals(64))
+  expect_that(nchar(sha256("foo")), equals(64))
 
 })
 
 test_that("SHA512 outputs a 128-character hash", {
 
-  expect_that(nchar(hash("foo","sha512")), equals(128))
+  expect_that(nchar(sha512("foo")), equals(128))
 
 })

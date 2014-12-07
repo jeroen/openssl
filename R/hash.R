17239b10e26643b3b7c1cf65f15b3d0903076893
Oliver Keyes <ironholds@gmail.com>
2014-12-07 15:12:48 -0500

    set dontrun flag around the example that requires downloading r-core, and modify it to add a file.remove() call. We should probably pick a checksum demonstration with a smaller file behind it.

--- R/hash.R
+++ R/hash.R
@@ -50,10 +50,13 @@
 #' # Use serialize to digest objects
 #' md5(serialize(cars, NULL))
 #'
+#' \dontrun{
 #' # Verify md5 from: http://cran.r-project.org/bin/windows/base/md5sum.txt
 #' tmp <- tempfile()
 #' download.file("http://cran.r-project.org/bin/windows/base/R-3.1.2-win.exe", tmp)
 #' md5(file(tmp))
+#' file.remove(tmp)
+#' }
 #'
 #' # Use a salt to prevent dictionary attacks
 #' sha1("admin") # googleable

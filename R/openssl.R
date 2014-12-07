e23e947bd734681cbbaf0943d2cf7a37e2379c4b
Oliver Keyes <ironholds@gmail.com>
2014-12-07 15:16:02 -0500

    add package-level documentation to get easy index access

---
+++ R/openssl.R
@@ -0,0 +1,10 @@
+#'@author Jeroen Ooms, Oliver Keyes
+#'@docType package
+#'@title R bindings for the OpenSSL library
+#'@description
+#'Interfaces to the OpenSSL libraries libssl and libcrypto.
+#'Currently only implements bindings to the OpenSSL secure random number
+#'generator and digest functions.
+#'@name openssl
+#'@aliases openssl openssl-package
+NULL

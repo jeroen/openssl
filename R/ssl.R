d83e0ad5ac14bd29f303db614295009d6c4e1d16
unknown <jeroenooms@gmail.com>
2015-07-24 14:32:49 +0200

    Doc fix

--- R/ssl.R
+++ R/ssl.R
@@ -1,5 +1,11 @@
+#' Download Certificate
+#'
+#' Connects to a HTTPS server and retrieves the SSL certificate.
+#'
 #' @useDynLib openssl R_download_cert
 #' @export
+#' @param host string: hostname of the server to connect to
+#' @param port integer: port to connect to
 download_cert <- function(host = "localhost", port = 443){
   stopifnot(is.character(host))
   stopifnot(is.numeric(port))

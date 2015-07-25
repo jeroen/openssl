0307e3eb4f0a2eca93835c0eeac11a16a448aefd
Jeroen Ooms <jeroenooms@gmail.com>
2015-07-25 15:03:53 +0200

    Tweak

--- src/error.c
+++ src/error.c
@@ -1,9 +1,10 @@
 #include <Rinternals.h>
 #include <openssl/err.h>
+#include "utils.h"
 
 void raise_error(){
   unsigned long err = ERR_get_error();
-  Rf_errorcall(R_NilValue, "OpenSSL error in %s: %s", ERR_func_error_string(err), ERR_reason_error_string(err));
+  stop("OpenSSL error in %s: %s", ERR_func_error_string(err), ERR_reason_error_string(err));
 }
 
 void bail(int success){
--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,4 @@
-#define stop(...) Rf_error(R_NilValue, __VA_ARGS__)
+#define stop(...) Rf_errorcall(R_NilValue, __VA_ARGS__)
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 void bail(int out);
 void raise_error();

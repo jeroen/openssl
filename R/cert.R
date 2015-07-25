e4ae2f8caecb9df7f9b51adbcaebcf220d75c6de
Jeroen Ooms <jeroenooms@gmail.com>
2015-07-25 16:28:36 +0200

    Add CA bundle

--- R/cert.R
+++ R/cert.R
@@ -5,7 +5,7 @@
 #' @export
 #' @useDynLib openssl R_certinfo
 #' @param cert a certificate
-#' @param ca a CA bundle
+#' @param root a root certificate or path to CA bundle
 #' @rdname certs
 certinfo <- function(cert){
   stopifnot(is.raw(cert))
@@ -16,8 +16,12 @@ certinfo <- function(cert){
 #' @export
 #' @rdname certs
 #' @name certs
-verify_cert <- function(cert, ca){
+verify_cert <- function(cert, root = system.file("cacert.pem", package = "openssl")){
   stopifnot(is.raw(cert))
-  stopifnot(is.raw(ca))
-  .Call(R_verify_cert, cert, ca)
+  if(is.character(root)){
+    root <- normalizePath(path.expand(root), mustWork = TRUE)
+  } else {
+    stopifnot(is.raw(root))
+  }
+  .Call(R_verify_cert, cert, root)
 }
--- src/cert.c
+++ src/cert.c
@@ -58,20 +58,25 @@ SEXP R_certinfo(SEXP bin){
 }
 
 SEXP R_verify_cert(SEXP certdata, SEXP cadata) {
-  const unsigned char *ptr;
-
+  /* load cert */
+  const unsigned char *ptr = RAW(certdata);
   X509 *cert = X509_new();
-  ptr = RAW(certdata);
+  X509 *ca = X509_new();
   bail(!!d2i_X509(&cert, &ptr, LENGTH(certdata)));
 
-  X509 *ca = X509_new();
-  ptr = RAW(cadata);
-  bail(!!d2i_X509(&ca, &ptr, LENGTH(cadata)));
-
+  /* init ca bundle store */
   X509_STORE *store = X509_STORE_new();
   X509_STORE_CTX *ctx = X509_STORE_CTX_new();
   X509_STORE_CTX_init(ctx, store, cert, NULL);
-  X509_STORE_add_cert(store, ca);
+
+  /* cadata is either path to bundle or cert */
+  if(isString(cadata)){
+    bail(X509_STORE_load_locations(store, CHAR(STRING_ELT(cadata, 0)), NULL));
+  } else {
+    ptr = RAW(cadata);
+    bail(!!d2i_X509(&ca, &ptr, LENGTH(cadata)));
+    bail(X509_STORE_add_cert(store, ca));
+  }
 
   if(X509_verify_cert(ctx) < 1)
     stop("Certificate validation failed: %s", X509_verify_cert_error_string(X509_STORE_CTX_get_error(ctx)));

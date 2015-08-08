e95a1d6e17e849f313c29e7dafaa6e56b3fd4105
Jeroen Ooms <jeroenooms@gmail.com>
2015-08-08 19:50:59 +0200

    Add bignum functions

---
+++ R/bignum.R
@@ -0,0 +1,151 @@
+#' Big number arithmetic
+#'
+#' Basic operations for working with large integers. The \code{bignum}
+#' funtion converts a positive integer, string or raw vector into a bignum type.
+#' All basic \link{Arithmetic} and \link{Comparison} operators such as
+#' \code{+}, \code{-}, \code{*}, \code{^}, \code{\%\%}, \code{\%/\%}, \code{==},
+#' \code{!=}, \code{<}, \code{<=}, \code{>} and \code{>=} are implemented for
+#' bignum objects.
+#'
+#' @export
+#' @name bignum
+#' @rdname bignum
+#' @param x an integer, string (hex or dec) or raw vector
+#' @param hex set to TRUE to parse strings as hex rather than decimal notation
+#' @useDynLib openssl R_parse_bignum
+#' @examples # create a bignum
+#' x <- bignum(123L)
+#' y <- bignum("123456789123456789")
+#' z <- bignum("D41D8CD98F00B204E9800998ECF8427E", hex = TRUE)
+#'
+#' # Basic arithmetic
+#' div <- z %% y
+#' mod <- z %/% y
+#' z2 <- div * y + mod
+#' stopifnot(z2 == z)
+#' stopifnot(div < z)
+bignum <- function(x, hex = FALSE){
+  if(inherits(x, "bignum"))
+    return(x)
+  stopifnot(is.raw(x) || is.character(x) || is.numeric(x))
+  if(is.numeric(x)){
+    if(is_positive_integer(x)){
+      x <- formatC(as.integer(x))
+    } else {
+      stop("Cannot convert to bignum: x must be positive integer, character or raw")
+    }
+  }
+  if(is.character(x)){
+    if(identical(x, "0")){
+      # special case always valid
+    } else if(isTRUE(hex)){
+      if(!grepl("^([a-fA-F0-9]{2})+$", x))
+        stop("Value '", x, "' is not valid hex string", call. = FALSE)
+    } else {
+      if(!grepl("^[0-9]+$", x))
+        stop("Value '", x, "' is not valid integer", call. = FALSE)
+    }
+  }
+  .Call(R_parse_bignum, x, hex)
+}
+
+bn <- bignum
+
+#' @export
+print.bignum <- function(x, hex = FALSE, ...){
+  cat("BN:", as.character.bignum(x, hex = hex))
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_as_character
+as.character.bignum <- function(x, hex = FALSE, ...){
+  .Call(R_bignum_as_character, x, hex)
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_add
+`+.bignum` <- function(x, y){
+  .Call(R_bignum_add, bn(x), bn(y))
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_subtract
+`-.bignum` <- function(x, y){
+  x <- bn(x)
+  y <- bn(y)
+  stopifnot(x > y)
+  .Call(R_bignum_subtract, x, y)
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_multiply
+`*.bignum` <- function(x, y){
+  .Call(R_bignum_multiply, bn(x), bn(y))
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_exp
+`^.bignum` <- function(x, y){
+  .Call(R_bignum_exp, bn(x), bn(y))
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_devide
+`%%.bignum` <- function(x, y){
+  .Call(R_bignum_devide, bn(x), bn(y))
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_mod
+`%/%.bignum` <- function(x, y){
+  .Call(R_bignum_mod, bn(x), bn(y))
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_compare
+`>.bignum` <- function(x, y){
+  identical(1L, .Call(R_bignum_compare, bn(x), bn(y)));
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_compare
+`<.bignum` <- function(x, y){
+  identical(-1L, .Call(R_bignum_compare, bn(x), bn(y)));
+}
+
+#' @export
+#' @useDynLib openssl R_bignum_compare
+`==.bignum` <- function(x, y){
+  identical(0L, .Call(R_bignum_compare, bn(x), bn(y)));
+}
+
+#' @export
+`!=.bignum` <- function(x, y){
+  !identical(0L, .Call(R_bignum_compare, bn(x), bn(y)));
+}
+
+#' @export
+`>=.bignum` <- function(x, y){
+  .Call(R_bignum_compare, bn(x), bn(y)) > -1L;
+}
+
+#' @export
+`<=.bignum` <- function(x, y){
+  .Call(R_bignum_compare, bn(x), bn(y)) < 1L;
+}
+
+#' @export
+`/.bignum` <- function(x, y){
+  stop("Use integer division %% and modulo %/% for dividing bignum objects", call. = FALSE)
+}
+
+is_positive_integer <- function(x)  {
+  if(x < 0)
+    return(FALSE)
+  if(is.integer(x))
+    return(TRUE)
+  tol <- sqrt(.Machine$double.eps)
+  if(x <= .Machine$integer.max && abs(x - round(x)) < tol)
+    return(TRUE)
+  return(FALSE)
+}
---
+++ src/bignum.c
@@ -0,0 +1,139 @@
+#include <Rinternals.h>
+#include "apple.h"
+#include "utils.h"
+#include <openssl/ec.h>
+#include <openssl/bn.h>
+#include <string.h>
+
+BIGNUM *r2bignum(SEXP x){
+  if(!inherits(x, "bignum"))
+    error("Argument is not valid bignum");
+  BIGNUM *val = BN_bin2bn(RAW(x), LENGTH(x), NULL);
+  bail(val != NULL);
+  return val;
+}
+
+SEXP bignum2r(BIGNUM *val){
+  SEXP out = PROTECT(allocVector(RAWSXP, BN_num_bytes(val)));
+  bail(BN_bn2bin(val, RAW(out)) >= 0);
+  setAttrib(out, R_ClassSymbol, mkString("bignum"));
+  UNPROTECT(1);
+  return out;
+}
+
+SEXP R_parse_bignum(SEXP x, SEXP hex){
+  BIGNUM *val = BN_new();
+  if(TYPEOF(x) == RAWSXP){
+    bail(NULL != BN_bin2bn(RAW(x), LENGTH(x), val));
+  } else if(asLogical(hex)){
+    bail(BN_hex2bn(&val, CHAR(STRING_ELT(x, 0))));
+  } else {
+    bail(BN_dec2bn(&val, CHAR(STRING_ELT(x, 0))));
+  }
+  SEXP res = bignum2r(val);
+  BN_free(val);
+  return res;
+}
+
+SEXP R_bignum_as_character(SEXP x, SEXP hex){
+  BIGNUM *val = r2bignum(x);
+  char *str;
+  if(asLogical(hex)){
+    bail(!!(str = BN_bn2hex(val)));
+  } else {
+    bail(!!(str = BN_bn2dec(val)));
+  }
+  SEXP res = mkString(str);
+  OPENSSL_free(str);
+  BN_free(val);
+  return res;
+}
+
+SEXP R_bignum_add(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  BIGNUM *out = BN_new();
+  bail(BN_add(out, val1, val2));
+  SEXP res = bignum2r(out);
+  BN_free(val1);
+  BN_free(val2);
+  BN_free(out);
+  return res;
+}
+
+SEXP R_bignum_subtract(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  BIGNUM *out = BN_new();
+  bail(BN_sub(out, val1, val2));
+  SEXP res = bignum2r(out);
+  BN_free(val1);
+  BN_free(val2);
+  BN_free(out);
+  return res;
+}
+
+SEXP R_bignum_multiply(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  BIGNUM *out = BN_new();
+  BN_CTX *ctx = BN_CTX_new();
+  bail(BN_mul(out, val1, val2, ctx));
+  SEXP res = bignum2r(out);
+  BN_free(val1);
+  BN_free(val2);
+  BN_free(out);
+  BN_CTX_free(ctx);
+  return res;
+}
+
+SEXP R_bignum_devide(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  BIGNUM *out = BN_new();
+  BN_CTX *ctx = BN_CTX_new();
+  bail(BN_div(out, NULL, val1, val2, ctx));
+  SEXP res = bignum2r(out);
+  BN_free(val1);
+  BN_free(val2);
+  BN_free(out);
+  BN_CTX_free(ctx);
+  return res;
+}
+
+SEXP R_bignum_mod(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  BIGNUM *out = BN_new();
+  BN_CTX *ctx = BN_CTX_new();
+  bail(BN_mod(out, val1, val2, ctx));
+  SEXP res = bignum2r(out);
+  BN_free(val1);
+  BN_free(val2);
+  BN_free(out);
+  BN_CTX_free(ctx);
+  return res;
+}
+
+SEXP R_bignum_exp(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  BIGNUM *out = BN_new();
+  BN_CTX *ctx = BN_CTX_new();
+  bail(BN_exp(out, val1, val2, ctx));
+  SEXP res = bignum2r(out);
+  BN_free(val1);
+  BN_free(val2);
+  BN_free(out);
+  BN_CTX_free(ctx);
+  return res;
+}
+
+SEXP R_bignum_compare(SEXP x, SEXP y){
+  BIGNUM *val1 = r2bignum(x);
+  BIGNUM *val2 = r2bignum(y);
+  int out = BN_cmp(val1, val2);
+  BN_free(val1);
+  BN_free(val2);
+  return ScalarInteger(out);
+}

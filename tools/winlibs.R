2e8a9ebc253ac5010d110f568a4ad13c431cff5c
Jeroen Ooms <jeroenooms@gmail.com>
2015-11-03 10:19:28 +0100

    Backport configure stuff

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
 # Build against openssl libraries that were compiled with the Rtools gcc toolchain.
 if(!file.exists("../windows/openssl-1.0.1j/include/openssl/ssl.h")){
-  setInternet2()
+  if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/openssl/archive/v1.0.1j.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")

09969bbb0c812c7f0a96f1b6bbab9af76fb2e843
Jeroen Ooms <jeroenooms@gmail.com>
2015-11-05 14:52:36 +0100

    Use proper padding for EC keys

--- R/openssh.R
+++ R/openssh.R
@@ -21,13 +21,15 @@ fingerprint.dsa <- function(pubkey){
 fingerprint.ecdsa <- function(pubkey){
   bindata <- ecdsa_decompose(pubkey)
   nist_name <- bindata[[1]]
-  ssh_name <- switch(nist_name,
-    "P-256" = "nistp256",
-    "P-384" = "nistp384",
-    "P-521" = "nistp521",
+  key_bits <- switch(nist_name,
+    "P-256" = 256,
+    "P-384" = 384,
+    "P-521" = 521,
     stop("Unknown curve type: ", nist_name)
   )
-  keydata <- c(as.raw(4), bindata[[2]], bindata[[3]])
+  key_size <- ceiling(key_bits/8)
+  ssh_name <- paste0("nistp", key_bits)
+  keydata <- c(as.raw(4), pad(bindata[[2]], key_size), pad(bindata[[3]], key_size))
   input <- c(list(charToRaw(paste0("ecdsa-sha2-", ssh_name))), list(charToRaw(ssh_name)), list(keydata))
   out <- lapply(input, function(x){
     c(writeBin(length(x), raw(), endian = "big"), x)
@@ -41,6 +43,12 @@ fingerprint.ed25519 <- function(pubkey){
     c(writeBin(length(x), raw(), endian = "big"), x)
   })
   md5(unlist(unname(out)))
+}
+
+# SSH integer blogs need fixed size but openssl drops leading zero's
+pad <- function(x, size){
+  dif <- size - length(x)
+  c(raw(dif), x)
 }
 
 #' @useDynLib openssl R_pubkey_type

3d8fa3012a2eef8201395065dbaa3e8edc528e77
Jeroen Ooms <jeroenooms@gmail.com>
2015-11-05 11:21:06 +0100

    Split write manual

--- R/write.R
+++ R/write.R
@@ -1,13 +1,34 @@
+#' Export key or certificate
+#'
+#' The \code{write_pem} functions exports a key or certificate to the standard
+#' base64 PEM format. For private keys it is possible to set a password.
+#'
 #' @export
-#' @rdname read_key
+#' @param x a public/private key or certificate object
+#' @param password string or callback function to set password (only applicable for
+#' private keys).
+#' @param path file to write to. If \code{NULL} it returns the output as a string.
+#' @rdname write_pem
 write_pem <- function(x, password = readline, path = NULL){
   str <- pem_export(x, password)
   if(is.null(path)) return(str)
   writeLines(str, path)
 }
 
+#' @export
+#' @rdname write_pem
+write_der <- function(x, path = NULL){
+  bin <- der_export(x)
+  if(is.null(path)) return(bin)
+  writeBin(unclass(bin), path)
+}
+
 pem_export <- function(x, ...){
   UseMethod("pem_export")
+}
+
+der_export <- function(x, ...){
+  UseMethod("der_export")
 }
 
 #' @useDynLib openssl R_pem_write_key
@@ -21,18 +42,6 @@ pem_export.key <- function(x, password, ...){
 #' @useDynLib openssl R_pem_write_pubkey
 pem_export.pubkey <- function(x, ...){
   .Call(R_pem_write_pubkey, x)
-}
-
-#' @export
-#' @rdname read_key
-write_der <- function(x, path = NULL){
-  bin <- der_export(x)
-  if(is.null(path)) return(bin)
-  writeBin(unclass(bin), path)
-}
-
-der_export <- function(x, ...){
-  UseMethod("der_export")
 }
 
 der_export.key <- function(x, ...){

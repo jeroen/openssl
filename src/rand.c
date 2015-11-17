a81b30d57c428b58ba7f31ee81b24d67ab8f3001
Jeroen Ooms <jeroenooms@gmail.com>
2015-11-18 00:29:15 +0100

    Fix ASAN

--- src/rand.c
+++ src/rand.c
@@ -7,6 +7,8 @@
 SEXP R_RAND_bytes(SEXP n, SEXP pseudo) {
   int use_pseudo = asLogical(pseudo);
   int length = asInteger(n);
+  if(length <= 0)
+    return allocVector(RAWSXP, 0);
   int result;
   unsigned char buf[length];
   if(use_pseudo){

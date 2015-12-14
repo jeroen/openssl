61c1a6ba661118c161d09400e389fda31f0c673f
Jeroen Ooms <jeroenooms@gmail.com>
2015-12-14 22:05:18 +0100

    Add my_key example

--- R/mykey.R
+++ R/mykey.R
@@ -1,13 +1,25 @@
 #' Default keypair
 #'
-#' The \code{my_key()} function looks for environment variable \code{USER_KEY} with a
-#' path to a private keyfile. If unset it defaults to \code{"~/.ssh/id_rsa"}. The
-#' \code{my_pubkey()} function looks for environment variable \code{USER_PUBKEY} which
-#' defaults to \code{"~/.ssh/id_rsa.pub"}. If neither exists it will also try \code{my_key()}
-#' and derive the corresponding public key.
+#' Set a default keypair via \code{USER_KEY} and \code{USER_PUKBEY} variables.
+#'
+#' The \code{my_key()} function checks environment variable \code{USER_KEY} for a
+#' path to a private keyfile. If unset it defaults to \code{"~/.ssh/id_rsa"}.
+#'
+#' The \code{my_pubkey()} function first tries \code{USER_PUBKEY} and if unset
+#' it checks for \code{USER_KEY} to derive the corresponding pubkey. If both are
+#' unset it defaults to \code{"~/.ssh/id_rsa.pub"}.
 #'
 #' @export
 #' @rdname my_key
+#' @examples # Set random RSA key as default
+#' key <- rsa_keygen()
+#' write_pem(key, tmp <- tempfile(), password = "")
+#' rm(key)
+#' Sys.setenv("USER_KEY" = tmp)
+#'
+#' # Check the new keys
+#' print(my_key())
+#' print(my_pubkey())
 my_key <- function(){
   path <- Sys.getenv("USER_KEY", "~/.ssh/id_rsa")
   if(!file.exists(path))

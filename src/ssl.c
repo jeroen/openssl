360a8689a159fc85f019adb97fac0233d0df67f2
Jeroen Ooms <jeroenooms@gmail.com>
2016-01-11 12:18:40 +0100

    Fix for windows

--- src/ssl.c
+++ src/ssl.c
@@ -8,6 +8,7 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
 #else
 #include <sys/socket.h>
 #include <resolv.h>

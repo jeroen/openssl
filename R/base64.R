8868f5891bc8e851bd28e396a527a490d401927d
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-04 16:23:00 +0100

    Fixes

--- R/base64.R
+++ R/base64.R
@@ -3,6 +3,11 @@
 #' Encode and decode binary data into a base64 string. Character vectors are
 #' automatically collapsed into a single string.
 #'
+#' The \code{base64url_encode} functions are specified in Section 5 of RFC 4648
+#' as a URL-safe alternative to the standard base64. They use different characters
+#' for the 62:nd and 63:rd alphabet character and don't include trailing \code{==}
+#' padding.
+#'
 #' @rdname base64_encode
 #' @useDynLib openssl R_base64_encode
 #' @param bin raw or character vector with data to encode into base64
@@ -31,12 +36,33 @@ base64_decode <- function(text){
   stopifnot(is.character(text))
   text <- paste(text, collapse="")
   text <- gsub("[\r\n]", "", text)[[1]]
+  .Call(R_base64_decode, text)
+}
 
-  # OpenSSL really needs proper padding
+# defined by rfc7515, rfc7519
+#' @rdname base64_encode
+#' @export
+base64url_encode <- function(bin){
+  text <- base64_encode(bin)
+  sub("=+$", "", chartr('+/', '-_', text))
+}
+
+#' @rdname base64_encode
+#' @export
+base64url_decode <- function(text){
+  text <- fix_padding(chartr('-_', '+/', text))
+  base64_decode(text)
+}
+
+
+
+# Ensures base64 length is a multiple of 4
+fix_padding <- function(text){
+  text <- gsub("[\r\n]", "", text)[[1]]
   mod <- nchar(text) %% 4;
   if(mod > 0){
     padding <- paste(rep("=", (4 - mod)), collapse = "")
     text <- paste0(text, padding)
   }
-  .Call(R_base64_decode, text)
+  text
 }

0247e485af75e1018b3ec259f609282b8d55a940
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-12 11:49:07 +0100

    Fix for OpenBSD

--- src/ssl.c
+++ src/ssl.c
@@ -11,6 +11,7 @@
 const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
 #else
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <resolv.h>
 #include <netdb.h>
 #include <arpa/inet.h>

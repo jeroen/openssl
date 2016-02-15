1a54e3af761813a9d7e000bf680f60f551b98c85
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-15 15:00:00 +0100

    Include key size in output

--- R/list.R
+++ R/list.R
@@ -5,6 +5,7 @@ as.list.key <- function(x, ...){
   pk <- as.list(pubkey)
   list(
     type = pk$type,
+    size = pk$size,
     pubkey = pubkey,
     data = decompose(key)
   )
@@ -15,6 +16,7 @@ as.list.pubkey <- function(x, ...){
   pubkey <- x
   data <- decompose(pubkey)
   type <- ifelse(inherits(pubkey, "ed25519"), "ed25519", pubkey_type(pubkey))
+  size <- pubkey_bitsize(pubkey)
   header <- switch(type,
    "rsa" = "ssh-rsa",
    "dsa" = "ssh-dss",
@@ -25,6 +27,7 @@ as.list.pubkey <- function(x, ...){
   fp <- unlist(unname(fpdata(pubkey)))
   list(
     type = type,
+    size = size,
     ssh = paste(header, base64_encode(fp)),
     fingerprint = md5(fp),
     data = data

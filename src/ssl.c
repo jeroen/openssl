c33029a102e52dfd83445189b0a431182d4c9c55
Jeroen Ooms <jeroenooms@gmail.com>
2016-08-12 10:52:31 +0200

    Don't need resolve and causes problems in c99

--- src/ssl.c
+++ src/ssl.c
@@ -12,7 +12,6 @@ const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <resolv.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #endif

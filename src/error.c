0885ff141fb4da17253d802d4926523022b82e33
Jeroen Ooms <jeroenooms@gmail.com>
2016-10-26 21:02:14 +0200

    Clear error buffers

--- src/error.c
+++ src/error.c
@@ -4,6 +4,7 @@
 
 void raise_error(){
   unsigned long err = ERR_get_error();
+  ERR_clear_error();
   stop("OpenSSL error in %s: %s", ERR_func_error_string(err), ERR_reason_error_string(err));
 }
 
--- tests/testthat/test_pkcs12.R
+++ tests/testthat/test_pkcs12.R
@@ -2,6 +2,8 @@ context("Test PKCS12 reader")
 
 test_that("reading p12 certificates", {
   p1 <- read_pkcs12("../google.dk/wildcard-google.dk-chain.p12")
+
+  expect_error(read_pkcs12("../google.dk/wildcard-google.dk-chain-password.p12", password = ""), "password")
   p2 <- read_pkcs12("../google.dk/wildcard-google.dk-chain-password.p12", password = "password")
   expect_identical(p1, p2)
 
@@ -10,20 +12,24 @@ test_that("reading p12 certificates", {
 
   leaf <- read_cert("../google.dk/wildcard-google.dk-leaf.crt", der = TRUE)
   expect_identical(leaf, bundle[[1]])
+
 })
 
 test_that("reading p12 keys", {
+  expect_error(read_pkcs12("../certigo/example-root.p12", password = ""), "password")
   b1 <- read_pkcs12("../certigo/example-root.p12", password = "password")
   c1 <- read_cert("../certigo/example-root.crt")
   expect_identical(b1$cert, c1)
   expect_identical(b1$cert$pubkey, b1$key$pubkey)
 
+  expect_error(read_pkcs12("../certigo/example-leaf.p12", password = ""), "password")
   b2<- read_pkcs12("../certigo/example-leaf.p12", password = "password")
   c2 <- read_cert("../certigo/example-leaf.crt")
   expect_identical(b2$cert, c2)
   expect_identical(b2$cert$pubkey, b2$key$pubkey)
 
   if(isTRUE(openssl_config()$ec)){
+    expect_error(read_pkcs12("../certigo/example-elliptic-sha1.p12", password = ""), "password")
     b3 <- read_pkcs12("../certigo/example-elliptic-sha1.p12", password = "password")
     c3 <- read_cert("../certigo/example-elliptic-sha1.crt")
     k3 <- read_key("../certigo/example-elliptic-sha1.key")

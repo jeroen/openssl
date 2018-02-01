a1bd931f51ca6057465bd16fd27e8f3f87c08ca3
Jeroen <jeroenooms@gmail.com>
2018-02-01 21:30:00 +0100

    More unit tests

--- tests/testthat/test_google.R
+++ tests/testthat/test_google.R
@@ -3,28 +3,28 @@ context("Google SSL tests")
 # Certificates from https://pki.goog/
 
 test_that("google certs validate", {
-  cert <- download_ssl_cert('good.r1demo.pki.goog')
-  ca <- read_cert('https://pki.goog/gtsr1/GTSR1.crt', der = TRUE)
-  expect_true(cert_verify(cert, ca))
+  # Google CA root certs
+  gtsr1 <- read_cert('https://pki.goog/gtsr1/GTSR1.crt', der = TRUE)
+  gtsr2 <- read_cert('https://pki.goog/gtsr2/GTSR2.crt', der = TRUE)
+  gtsr3 <- read_cert('https://pki.goog/gtsr3/GTSR3.crt', der = TRUE)
+  gtsr4 <- read_cert('https://pki.goog/gtsr4/GTSR4.crt', der = TRUE)
+  gsr2 <- read_cert('https://pki.goog/gsr2/GSR2.crt', der = TRUE)
+  gsr4 <- read_cert('https://pki.goog/gsr4/GSR4.crt', der = TRUE)
 
-  cert <- download_ssl_cert('good.r2demo.pki.goog')
-  ca <- read_cert('https://pki.goog/gtsr2/GTSR2.crt', der = TRUE)
-  expect_true(cert_verify(cert, ca))
+  # Test good servers
+  expect_true(cert_verify(download_ssl_cert('good.r1demo.pki.goog'), gtsr1))
+  expect_true(cert_verify(download_ssl_cert('good.r2demo.pki.goog'), gtsr2))
+  expect_true(cert_verify(download_ssl_cert('good.r3demo.pki.goog'), gtsr3))
+  expect_true(cert_verify(download_ssl_cert('good.r4demo.pki.goog'), gtsr4))
+  #expect_true(cert_verify(download_ssl_cert('2021.globalsign.com'), gsr2))
+  expect_true(cert_verify(download_ssl_cert('2038r4.globalsign.com'), gsr4))
 
-  cert <- download_ssl_cert('good.r3demo.pki.goog')
-  ca <- read_cert('https://pki.goog/gtsr3/GTSR3.crt', der = TRUE)
-  expect_true(cert_verify(cert, ca))
+  # Test expired servers
+  expect_error(cert_verify(download_ssl_cert('expired.r1demo.pki.goog'), gtsr1), 'expired')
+  expect_error(cert_verify(download_ssl_cert('expired.r2demo.pki.goog'), gtsr2), 'expired')
+  expect_error(cert_verify(download_ssl_cert('expired.r3demo.pki.goog'), gtsr3), 'expired')
+  expect_error(cert_verify(download_ssl_cert('expired.r4demo.pki.goog'), gtsr4), 'expired')
+  expect_error(cert_verify(download_ssl_cert('expired.gsr2demo.pki.goog'), gsr2), 'expired')
+  expect_error(cert_verify(download_ssl_cert('expired.gsr4demo.pki.goog'), gsr4), 'expired')
 
-  cert <- download_ssl_cert('good.r4demo.pki.goog')
-  ca <- read_cert('https://pki.goog/gtsr4/GTSR4.crt', der = TRUE)
-  expect_true(cert_verify(cert, ca))
-
-  # Google messed up, server doesn't work
-  #cert <- download_ssl_cert('2021.globalsign.com')
-  #ca <- read_cert('https://pki.goog/gsr2/GSR2.crt')
-  #expect_true(cert_verify(cert, ca))
-
-  cert <- download_ssl_cert('2038r4.globalsign.com')
-  ca <- read_cert('https://pki.goog/gsr4/GSR4.crt', der = TRUE)
-  expect_true(cert_verify(cert, ca))
 })

596079d1b282ce61c54fd51be7133d2c728e6f67
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-15 12:37:33 +0100

    Update default key for Windows

--- R/mykey.R
+++ R/mykey.R
@@ -1,13 +1,13 @@
-#' Default keypair
+#' Default key
 #'
-#' Set a default keypair via \code{USER_KEY} and \code{USER_PUKBEY} variables.
+#' The default user key can be set in the \code{USER_KEY} variable and otherwise
+#' is \code{~/.ssh/id_rsa}. Note that on Windows we treat `~` as the windows user
+#' home (and not the documents folder).
 #'
-#' The \code{my_key()} function checks environment variable \code{USER_KEY} for a
-#' path to a private keyfile. If unset it defaults to \code{"~/.ssh/id_rsa"}.
-#'
-#' The \code{my_pubkey()} function first tries \code{USER_PUBKEY} and if unset
-#' it checks for \code{USER_KEY} to derive the corresponding pubkey. If both are
-#' unset it defaults to \code{"~/.ssh/id_rsa.pub"}.
+#' The \code{my_pubkey()} function looks for the public key by appending \code{.pub}
+#' to the above key path. If this file does not exist, it reads the private key file
+#' and automatically derives the corresponding pubkey. In the latter case the user
+#' may be prompted for a passphrase if the private key is protected.
 #'
 #' @export
 #' @rdname my_key
@@ -21,7 +21,7 @@
 #' print(my_key())
 #' print(my_pubkey())
 my_key <- function(){
-  path <- Sys.getenv("USER_KEY", "~/.ssh/id_rsa")
+  path <- default_key()
   if(!file.exists(path))
     stop("No suitable user key found.")
   read_key(path)
@@ -30,11 +30,31 @@ my_key <- function(){
 #' @export
 #' @rdname my_key
 my_pubkey <- function(){
-  path <- Sys.getenv("USER_PUBKEY", Sys.getenv("USER_KEY", "~/.ssh/id_rsa.pub"))
+  path <- paste0(default_key(), ".pub")
   if(file.exists(path))
     return(read_pubkey(path))
 
   # alternatively derive pubkey from key
   key <- my_key()
-  key$pubkey
+  pubkey <- key$pubkey
+  try(write_ssh(pubkey, path), silent = TRUE)
+  pubkey
+}
+
+default_key <- function(){
+  normalize_home(Sys.getenv("USER_KEY", "~/.ssh/id_rsa"))
+}
+
+normalize_home <- function(path = NULL){
+  path <- as.character(path)
+  if(is_windows()){
+    homedir <- Sys.getenv('USERPROFILE')
+    is_home <- grepl("^~", path)
+    path[is_home] <- paste0(homedir, substring(path[is_home], 2))
+  }
+  normalizePath(path, mustWork = FALSE)
+}
+
+is_windows <- function(){
+  .Platform$OS.type == "windows"
 }

8829f5b4a6adf84d395be9fc8eef4f01caed8c49
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-16 15:26:14 +0100

    Use applescript for password entry

--- R/askpass.R
+++ R/askpass.R
@@ -9,8 +9,22 @@
 askpass <- function(prompt = "Please enter your password: "){
   if(!interactive())
     return(NULL)
-  FUN <- getOption("askpass", readline_silent)
+  FUN <- getOption("askpass", ask_password_default)
   FUN(prompt)
+}
+
+ask_password_default <- function(prompt){
+  if(is_macos() && !isatty(stdin())){
+    applescript_password(prompt)
+  } else {
+    readline_silent(prompt)
+  }
+}
+
+applescript_password <- function(prompt){
+  mac_askpass <- system.file('mac-askpass', package = 'openssl', mustWork = TRUE)
+  res <- sys::exec_internal(mac_askpass, prompt)
+  out_without_eol(res$stdout)
 }
 
 readline_silent <- function(prompt){
@@ -39,3 +53,6 @@ is_macos <- function(){
   identical(tolower(Sys.info()[['sysname']]), "darwin")
 }
 
+out_without_eol <- function(x){
+  sub("\n$", "", rawToChar(x))
+}

aef5fc75461e2d95587e6cc50acd5693463c8de1
Jeroen Ooms <jeroenooms@gmail.com>
2020-07-01 14:52:29 +0200

    Fixes and workarounds for OpenSSLv3
    Revert RSA workaround, problem fixed upstream.
    
    Update travis URL
    
    Fix for Windows
    
    Fix merge conflicts

--- src/onload.c
+++ src/onload.c
@@ -5,10 +5,17 @@
 #include <openssl/engine.h>
 #include <openssl/hmac.h>
 
+#if defined(OPENSSL_VERSION_MAJOR) && OPENSSL_VERSION_MAJOR >= 3
+#define HAS_OPENSSL3_API 1
+#endif
+
+#ifdef HAS_OPENSSL3_API
+#include <openssl/provider.h>
+#endif
+
 #ifdef _WIN32
 #include <winsock2.h>
 #endif
-
 
 void R_init_openssl(DllInfo *info) {
   R_registerRoutines(info, NULL, NULL, NULL, NULL);
@@ -16,6 +23,10 @@ void R_init_openssl(DllInfo *info) {
 #ifdef _WIN32
   WSADATA wsaData;
   WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
+#ifdef HAS_OPENSSL3_API
+  OSSL_PROVIDER_load(NULL, "legacy");
+  OSSL_PROVIDER_load(NULL, "default");
 #endif
   OpenSSL_add_all_digests();
   OpenSSL_add_all_algorithms();

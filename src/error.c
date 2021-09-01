1d5b42cf652c223727f815b687f997a6efb82dc9
Jeroen Ooms <jeroenooms@gmail.com>
2021-09-01 13:09:59 +0200

    Only use new error functions for OpenSSL3 for now

--- src/error.c
+++ src/error.c
@@ -1,15 +1,27 @@
 #include <Rinternals.h>
 #include <openssl/err.h>
+#include <string.h>
 #include "utils.h"
 
+/* Todo: try to use same error format for OpenSSL 1.1. and OpenSSL 3.0 */
+#if defined(OPENSSL_VERSION_MAJOR) && OPENSSL_VERSION_MAJOR >= 3
+static int copy_err_to_buf(const char *str, size_t len, void *u){
+  char *dst = u;
+  strncpy(dst, str, len);
+  return len;
+}
 void raise_error(){
   char buf[8192] = {0};
-  BIO *bp = BIO_new(BIO_s_mem());
-  ERR_print_errors(bp);
-  int len = BIO_read(bp, buf, 8192);
-  BIO_free(bp);
-  Rf_error("OpenSSL error: %.*s", len, buf);
+  ERR_print_errors_cb(copy_err_to_buf, buf); /* Only keeps the last error from the stack */
+  Rf_error("OpenSSL error: %s", buf);
 }
+#else
+void raise_error(){
+  unsigned long err = ERR_get_error(); //Pops earliest error from the queue
+  ERR_clear_error(); //Removes additional errors (if any) from the queue
+  stop("OpenSSL error in %s: %s", ERR_func_error_string(err), ERR_reason_error_string(err));
+}
+#endif
 
 void bail(int success){
   if(!success)

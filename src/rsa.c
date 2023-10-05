5302c6fc737aad45808be489bdf72297bf942321
Jason Law <jasonelaw@users.noreply.github.com>
2023-10-05 14:22:18 -0700

    Added support for OAEP padding to RSA encryption/decryption. (#120)
    Co-authored-by: Jason Law <jason.law@portlandoregon.gov>

--- src/rsa.c
+++ src/rsa.c
@@ -5,13 +5,14 @@
 #include <openssl/pem.h>
 #include "utils.h"
 
-SEXP R_rsa_encrypt(SEXP data, SEXP keydata) {
+SEXP R_rsa_encrypt(SEXP data, SEXP keydata, SEXP oaep) {
   const unsigned char *ptr = RAW(keydata);
   RSA *rsa = d2i_RSA_PUBKEY(NULL, &ptr, LENGTH(keydata));
+  int pad = asLogical(oaep) ? RSA_PKCS1_OAEP_PADDING : RSA_PKCS1_PADDING;
   bail(!!rsa);
   int keysize = RSA_size(rsa);
   unsigned char* buf = OPENSSL_malloc(keysize);
-  int len = RSA_public_encrypt(LENGTH(data), RAW(data), buf, rsa, RSA_PKCS1_PADDING);
+  int len = RSA_public_encrypt(LENGTH(data), RAW(data), buf, rsa, pad);
   bail(len > 0);
   RSA_free(rsa);
   SEXP res = allocVector(RAWSXP, len);
@@ -20,13 +21,14 @@ SEXP R_rsa_encrypt(SEXP data, SEXP keydata) {
   return res;
 }
 
-SEXP R_rsa_decrypt(SEXP data, SEXP keydata){
+SEXP R_rsa_decrypt(SEXP data, SEXP keydata, SEXP oaep){
   const unsigned char *ptr = RAW(keydata);
   RSA *rsa = d2i_RSAPrivateKey(NULL, &ptr, LENGTH(keydata));
+  int pad = asLogical(oaep) ? RSA_PKCS1_OAEP_PADDING : RSA_PKCS1_PADDING;
   bail(!!rsa);
   int keysize = RSA_size(rsa);
   unsigned char* buf = OPENSSL_malloc(keysize);
-  int len = RSA_private_decrypt(LENGTH(data), RAW(data), buf, rsa, RSA_PKCS1_PADDING);
+  int len = RSA_private_decrypt(LENGTH(data), RAW(data), buf, rsa, pad);
   bail(len > 0);
   RSA_free(rsa);
   SEXP res = allocVector(RAWSXP, len);

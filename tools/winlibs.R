77bee5117e3e281c50fe6d1a999438624089204f
Jeroen Ooms <jeroenooms@gmail.com>
2024-05-16 16:04:11 +0200

    Windows: update to OpenSSL 3.3.0

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists("../windows/libssl/include/openssl/pem.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/openssl-3.1.2/openssl-3.1.2-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/openssl-3.3.0/openssl-3.3.0-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/openssl-3.1.2/openssl-3.1.2-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/openssl-3.3.0/openssl-3.3.0-clang-x86_64.tar.xz"
   }  else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/openssl-3.1.2/openssl-3.1.2-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/openssl-3.3.0/openssl-3.3.0-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/openssl/archive/v3.1.1.tar.gz"
   }

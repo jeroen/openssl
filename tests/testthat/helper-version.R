5fbe62a9149e9d4c30f5ad3896dab8930c6eb27d
Jeroen Ooms <jeroenooms@gmail.com>
2025-05-26 11:03:31 +0200

    Fix for BDR setup which does not have vendor=redhat

--- tests/testthat/helper-version.R
+++ tests/testthat/helper-version.R
@@ -1,4 +1,4 @@
 # See https://github.com/jeroen/openssl/issues/125
 skip_on_redhat <- function(){
-  skip_if(grepl("redhat", R.version$platform))
+  skip_if(any(grepl("fedora|redhat", c(osVersion, R.version$platform), ignore.case = TRUE)))
 }

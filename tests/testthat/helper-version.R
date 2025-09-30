0eaeba8143a30ae049619daaadc5aa3195b9e108
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-30 09:58:34 +0200

    Fix for Rocky9

--- tests/testthat/helper-version.R
+++ tests/testthat/helper-version.R
@@ -1,4 +1,4 @@
 # See https://github.com/jeroen/openssl/issues/125
 skip_on_redhat <- function(){
-  skip_if(any(grepl("fedora|redhat", c(osVersion, R.version$platform), ignore.case = TRUE)))
+  skip_if(any(grepl("fedora|redhat|rocky", c(osVersion, R.version$platform), ignore.case = TRUE)))
 }

701eb29efb03e3544891178ca17d2fed3aa6df55
Jeroen Ooms <jeroenooms@gmail.com>
2026-02-26 13:39:33 +0100

    Force loading windows bcrypt Closes https://github.com/jeroen/bcrypt/issues/7

--- R/init.R
+++ R/init.R
@@ -5,6 +5,12 @@ libssl_version_number <- NULL
     m <- regexec('[0-9]+\\.[0-9]+\\.[0-9]+', ver)
     libssl_version_number <<- numeric_version(regmatches(ver, m)[[1]])
   })
+  if(.Platform$OS.type == 'windows'){
+    # See https://github.com/jeroen/bcrypt/issues/7
+    if(file.exists("C:\\Windows\\System32\\bcrypt.dll")){
+      dyn.load("C:\\Windows\\System32\\bcrypt.dll")
+    }
+  }
 }
 
 .onAttach <- function(libname, pkg){
